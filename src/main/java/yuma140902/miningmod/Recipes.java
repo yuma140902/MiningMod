7c266304f4c8eb3a18b99650a9628a093fcdf371
yuma140902 <yuma140902@gmail.com>
2018-08-17 17:42:21 +0900

    add 圧縮土ブロックのレシピ

--- src/main/java/yuma140902/miningmod/Recipes.java
+++ src/main/java/yuma140902/miningmod/Recipes.java
@@ -4,14 +4,15 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import yuma140902.miningmod.blocks.CompressCobblestoneBlock;
+import yuma140902.miningmod.blocks.CompressDirtBlock;
 
 public final class Recipes {
 	private Recipes() {}
 	public static void register() {
-		final int MAX = CompressCobblestoneBlock.MAX;
+		final int COBBLESTONE_MAX = CompressCobblestoneBlock.MAX;
 		
-		ItemStack[] compressCobblestoneBlockTmp1 = new ItemStack[MAX];
-		for(int i = 0; i < MAX; ++i) {
+		ItemStack[] compressCobblestoneBlockTmp1 = new ItemStack[COBBLESTONE_MAX];
+		for(int i = 0; i < COBBLESTONE_MAX; ++i) {
 			compressCobblestoneBlockTmp1[i] = new ItemStack(MiningMod.compressCobblestoneBlock, 1, i);
 		}
 		
@@ -21,7 +22,7 @@ public final class Recipes {
 				Blocks.cobblestone,
 				Blocks.cobblestone);
 		
-		for(int i = 1; i < MAX; ++i) {
+		for(int i = 1; i < COBBLESTONE_MAX; ++i) {
 			GameRegistry.addShapelessRecipe(compressCobblestoneBlockTmp1[i],
 					compressCobblestoneBlockTmp1[i-1],
 					compressCobblestoneBlockTmp1[i-1],
@@ -32,17 +33,56 @@ public final class Recipes {
 		
 		
 		
-		ItemStack[] compressCobblestoneBlockTmp4 = new ItemStack[MAX];
-		for(int i = 0; i < MAX; ++i) {
+		ItemStack[] compressCobblestoneBlockTmp4 = new ItemStack[COBBLESTONE_MAX];
+		for(int i = 0; i < COBBLESTONE_MAX; ++i) {
 			compressCobblestoneBlockTmp4[i]= new ItemStack(MiningMod.compressCobblestoneBlock, 4, i); 
 		}
 		
 		GameRegistry.addShapelessRecipe(new ItemStack(Blocks.cobblestone, 4), 
 				compressCobblestoneBlockTmp4[0]);
 		
-		for(int i = MAX - 1; i >= 1; --i) {
+		for(int i = COBBLESTONE_MAX - 1; i >= 1; --i) {
 			GameRegistry.addShapelessRecipe(compressCobblestoneBlockTmp4[i-1], 
 					compressCobblestoneBlockTmp4[i]);
+		}
+		
+		
+		
+		final int DIRT_MAX = CompressDirtBlock.MAX;
+		
+		ItemStack[] compressDirtBlockTmp1 = new ItemStack[DIRT_MAX];
+		for(int i = 0; i < DIRT_MAX; ++i) {
+			compressDirtBlockTmp1[i] = new ItemStack(MiningMod.compressDirtBlock, 1, i);
+		}
+		
+		GameRegistry.addShapelessRecipe(compressDirtBlockTmp1[0],
+				Blocks.dirt,
+				Blocks.dirt,
+				Blocks.dirt,
+				Blocks.dirt);
+		
+		for(int i = 1; i < DIRT_MAX; ++i) {
+			GameRegistry.addShapelessRecipe(compressDirtBlockTmp1[i],
+					compressDirtBlockTmp1[i-1],
+					compressDirtBlockTmp1[i-1],
+					compressDirtBlockTmp1[i-1],
+					compressDirtBlockTmp1[i-1]
+					);
+		}
+		
+		
+		
+		ItemStack[] compressDirtBlockTmp4 = new ItemStack[DIRT_MAX];
+		for(int i = 0; i < DIRT_MAX; ++i) {
+			compressDirtBlockTmp4[i]= new ItemStack(MiningMod.compressDirtBlock, 4, i); 
+		}
+		
+		GameRegistry.addShapelessRecipe(new ItemStack(Blocks.dirt, 4), 
+				compressDirtBlockTmp4[0]);
+		
+		for(int i = DIRT_MAX - 1; i >= 1; --i) {
+			GameRegistry.addShapelessRecipe(compressDirtBlockTmp4[i-1], 
+					compressDirtBlockTmp4[i]);
 		}
 	}
 }
