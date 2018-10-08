9eb7d123927c5137031d28ed31a6871a90b5c5c0
yuma140902 <yuma140902@gmail.com>
2018-10-08 18:42:03 +0900

    abstractなItemTorchToolをなくし、ユーティリティメソッドからツルハシ、剣などの各種ツールがonItemUseを呼び出すようにした

--- src/main/java/yuma140902/miningmod/util/TorchUtil.java
+++ src/main/java/yuma140902/miningmod/util/TorchUtil.java
@@ -1,25 +1,18 @@
-package yuma140902.miningmod.items;
+package yuma140902.miningmod.util;
 
-import java.util.Set;
 import cpw.mods.fml.common.registry.GameData;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemTool;
 import net.minecraft.stats.StatList;
 import net.minecraft.world.World;
-import yuma140902.miningmod.blocks.IRegisterable;
 
-public abstract class ItemTorchTool extends ItemTool implements IRegisterable{
-
+public class TorchUtil {
+	private TorchUtil() {}
 	
-	protected ItemTorchTool(float p_i45333_1_, ToolMaterial p_i45333_2_, Set p_i45333_3_) {
-		super(p_i45333_1_, p_i45333_2_, p_i45333_3_);
-	}
-
-	private boolean isTorch(Item item) {
+	public static boolean isTorch(Item item) {
 		String[] torchNameList =
 			{
 				"minecraft:torch"
@@ -32,7 +25,7 @@ public abstract class ItemTorchTool extends ItemTool implements IRegisterable{
 		return false;
 	}
 	
-	private int getTorchInInventorySlots(EntityPlayer player) {
+	public static int getTorchInInventorySlots(EntityPlayer player) {
 		for(int i = 0; i <= 35; ++i) { //インベントリはホットバーも含めて36個。ホットバーの一番左が0である。
 			ItemStack itemStack = player.inventory.getStackInSlot(i);
 			if(itemStack != null && isTorch(itemStack.getItem())) {
@@ -42,8 +35,7 @@ public abstract class ItemTorchTool extends ItemTool implements IRegisterable{
 		return -1;
 	}
 	
-	@Override
-	public boolean onItemUse(ItemStack itemStack, EntityPlayer player, World world, int x, int y, int z, int side, float par8, float par9, float par10) {
+	public static boolean onItemUse(ItemStack itemStack, EntityPlayer player, World world, int x, int y, int z, int side, float par8, float par9, float par10) {
 		if(!world.isRemote) {
 			int torchSlot = getTorchInInventorySlots(player);
 			if(torchSlot < 0) { return false; }
@@ -58,5 +50,4 @@ public abstract class ItemTorchTool extends ItemTool implements IRegisterable{
 		
 		return false;
 	}
-	
 }
